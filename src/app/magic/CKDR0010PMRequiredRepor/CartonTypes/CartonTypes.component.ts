a5ebbe7657e95639dc6aa5f4c2022c2fa3e25392
Suraj <tilak.raj1@honda.hmsi.in>
2026-05-18 17:01:15 +0530

    #Packing plan print changes.

--- src/app/magic/CKDR0010PMRequiredRepor/CartonTypes/CartonTypes.component.ts
+++ src/app/magic/CKDR0010PMRequiredRepor/CartonTypes/CartonTypes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { FormGroup } from "@angular/forms";
+import { FormControl, FormGroup } from "@angular/forms";
 import { MgFormControlsAccessor, MgControlName, MgCustomProperties } from "./CartonTypes.mg.controls.g";
 import { MgDisplayedColumns } from "./CartonTypes.mg.controls.g";
 
@@ -24,4 +24,44 @@ export class CartonTypes extends BaseMatTableMagicComponent {
         this.mgfc = new MgFormControlsAccessor(formGroup, this.magicServices);
     }
     override displayedColumns = this.mgdp;
+    override ngOnInit(): void {
+        super.ngOnInit();
+        const group: FormGroup = this.screenFormGroup;
+        (group.controls['vBlob64base'] as FormControl).registerOnChange(this.OnChange.bind(this));
+      }
+
+     OnChange() {
+        if (
+          this.mg.getValue('vBlob64base') !== undefined &&
+          this.mg.getValue('vBlob64base') !== null
+        ) {
+          this.downloadblb2();
+        }
+      }
+      downloadblb2() {
+        const base64 = this.mg.getValue('vBlob64base');
+        if (base64 !== undefined || base64 !== null) {
+          const byteCharacters = atob(base64);
+          const byteNumbers = new Array(byteCharacters.length);
+    
+          for (let i = 0; i < byteCharacters.length; i++) {
+            byteNumbers[i] = byteCharacters.charCodeAt(i);
+          }
+    
+          const byteArray = new Uint8Array(byteNumbers);
+          const binLb2 = new window.Blob([byteArray]);
+          if (binLb2.size !== 0) {
+            const downloadlink = document.createElement('a');
+            const filename = this.mg.getValue(this.mgc.vFileName);
+            const linkSource = window.URL.createObjectURL(binLb2);
+    
+            downloadlink.href = linkSource;
+            downloadlink.download = filename;
+            downloadlink.click();
+            window.URL.revokeObjectURL(linkSource);
+          }
+        }
+    
+      }
 }
+    
