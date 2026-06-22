1cad598e760c33159adc14661fdc97268169375c
Suraj <tilak.raj1@honda.hmsi.in>
2026-06-22 18:39:45 +0530

    #Button Size Adjustment.

--- src/app/magic/CKDR0050CKDDailyPackReport/CKDDAILYREPORT.component.ts
+++ src/app/magic/CKDR0050CKDDailyPackReport/CKDDAILYREPORT.component.ts
@@ -27,8 +27,8 @@ export class CKDDAILYREPORT extends TaskBaseMagicComponent implements MagicModal
     private static readonly showTitleBar: boolean = false;
     private static readonly x: number = 0;
     private static readonly y: number = 0;
-    private static readonly width: string = "600px";
-    private static readonly height: string = "400px";
+    private static readonly width: string = "500px";
+    private static readonly height: string = "300px";
     private static readonly isCenteredToWindow: boolean = true;
     private static readonly shouldCloseOnBackgroundClick: boolean = false;
     private static readonly isResizable: boolean = true;
